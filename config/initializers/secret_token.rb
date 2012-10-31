99649352752bee86078640524c2a7d0f2b5ded9c
Vadim Ribak <vadim.ribak@sap.com>
2012-10-31 11:58:18 +0200

    Start project in Ruby Mine

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fb::Application.config.secret_token = 'cbf18b6246c2df02fd9cea11ed28cc7082c383a11cb240a7760a0c6613071898a72c2d68bd555e39b8745759ce8121eff4ecc58866698c3df53e9cda64780287'
